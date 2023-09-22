d1be46ac7a0e31dec74b2e179a6b52044088e330
Sean Smith <sean@wwsean08.com>
2023-09-22 10:18:56 -0700

    Initial templating of a cobra application

---
+++ cmd/root.go
@@ -0,0 +1,43 @@
+/*
+Copyright © 2023 NAME HERE <EMAIL ADDRESS>
+*/
+package cmd
+
+import (
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+// rootCmd represents the base command when called without any subcommands
+var rootCmd = &cobra.Command{
+	Use:   "actions-dependency-graph",
+	Short: "An application to determine the dependencies of your actions/workflows",
+	Long: `An application that recursively walks actions and workflows to determine 
+the list of dependent actions so you can feel confident in what you're shipping and 
+allow you to identify problematic actions that you may want to avoid.'`,
+	// Uncomment the following line if your bare application
+	// has an action associated with it:
+	// Run: func(cmd *cobra.Command, args []string) { },
+}
+
+// Execute adds all child commands to the root command and sets flags appropriately.
+// This is called by main.main(). It only needs to happen once to the rootCmd.
+func Execute() {
+	err := rootCmd.Execute()
+	if err != nil {
+		os.Exit(1)
+	}
+}
+
+func init() {
+	// Here you will define your flags and configuration settings.
+	// Cobra supports persistent flags, which, if defined here,
+	// will be global for your application.
+
+	// rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.actions-dependency-graph.yaml)")
+
+	// Cobra also supports local flags, which will only run
+	// when this action is called directly.
+	//rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}

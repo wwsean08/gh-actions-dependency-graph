b9115de6805842040bf3eae2ed91ddb6ec2787c9
Sean Smith <sean@wwsean08.com>
2023-10-08 23:15:45 -0700

    Add some docs and one new method to implement

--- pkg/action/action.go
+++ pkg/action/action.go
@@ -11,17 +11,20 @@ import (
 	"sigs.k8s.io/yaml"
 )
 
+// Action a simplistic representation of a GitHub Action
 type Action struct {
 	Name        *string    `yaml:"name"`
 	Description *string    `yaml:"description"`
 	Runs        *RunsBlock `yaml:"runs"`
 }
 
+// RunsBlock represents the runs section of a GitHub Action
 type RunsBlock struct {
 	Using string         `yaml:"using"`
 	Steps []*common.Step `yaml:"steps"`
 }
 
+// ParseAction parses an action file and returns an Action.
 func ParseAction(file string) (*Action, error) {
 	if _, err := os.Stat(file); err != nil {
 		return nil, err
@@ -42,14 +45,24 @@ func ParseAction(file string) (*Action, error) {
 	return action, err
 }
 
+// GetActionFromRepoPath downloads the action.yaml file from a repository at a specific
+// git ref and parses it into an Action struct
+func GetActionFromRepoPath(owner, repo, ref, path string) (*Action, error) {
+	return nil, errors.New("TODO: Implement")
+}
+
+// IsComposite returns true if the Action is a composite action
 func (a *Action) IsComposite() bool {
 	return a.Runs.Using == "composite"
 }
 
+// IsJavascript returns true if the Action is a javascript action
 func (a *Action) IsJavascript() bool {
 	return strings.HasPrefix(a.Runs.Using, "node")
 }
 
+// GetNodeVersion returns the version of Node.js the Action runs on if it is a javascript
+// Action
 func (a *Action) GetNodeVersion() (int, error) {
 	if !a.IsJavascript() {
 		return 0, errors.New("action is not a javascript action")
@@ -64,6 +77,7 @@ func (a *Action) GetNodeVersion() (int, error) {
 	return intVersion, nil
 }
 
+// IsDocker returns true is the Action is a docker action
 func (a *Action) IsDocker() bool {
 	return a.Runs.Using == "docker"
 }

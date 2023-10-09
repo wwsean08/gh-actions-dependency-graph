c4ba91c09ae4966de3bbb1f8c95d9c794f2e7114
Sean Smith <sean@wwsean08.com>
2023-10-08 22:31:33 -0700

    Create initial action parsing and some methods to go with it

--- pkg/action/action.go
+++ pkg/action/action.go
@@ -1,13 +1,69 @@
 package action
 
-import "fmt"
+import (
+	"errors"
+	"io"
+	"os"
+	"strconv"
+	"strings"
+
+	"github.com/wwsean08/actions-dependency-graph/pkg/common"
+	"sigs.k8s.io/yaml"
+)
 
 type Action struct {
+	Name        *string    `yaml:"name"`
+	Description *string    `yaml:"description"`
+	Runs        *RunsBlock `yaml:"runs"`
 }
 
-type CompositeAction struct {
+type RunsBlock struct {
+	Using string         `yaml:"using"`
+	Steps []*common.Step `yaml:"steps"`
 }
 
 func ParseAction(file string) (*Action, error) {
-	return nil, fmt.Errorf("TODO: Implement")
+	if _, err := os.Stat(file); err != nil {
+		return nil, err
+	}
+	r, err := os.Open(file)
+	if err != nil {
+		return nil, err
+	}
+	defer r.Close()
+
+	data, err := io.ReadAll(r)
+	if err != nil {
+		return nil, err
+	}
+
+	action := new(Action)
+	err = yaml.Unmarshal(data, action)
+	return action, err
+}
+
+func (a *Action) IsComposite() bool {
+	return a.Runs.Using == "composite"
+}
+
+func (a *Action) IsJavascript() bool {
+	return strings.HasPrefix(a.Runs.Using, "node")
+}
+
+func (a *Action) GetNodeVersion() (int, error) {
+	if !a.IsJavascript() {
+		return 0, errors.New("action is not a javascript action")
+	}
+
+	textVersion, _ := strings.CutPrefix(a.Runs.Using, "node")
+	intVersion, err := strconv.Atoi(textVersion)
+	if err != nil {
+		return 0, err
+	}
+
+	return intVersion, nil
+}
+
+func (a *Action) IsDocker() bool {
+	return a.Runs.Using == "docker"
 }
--- pkg/action/action_test.go
+++ pkg/action/action_test.go
@@ -3,12 +3,209 @@ package action
 import (
 	"testing"
 
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
 
-// Some initial TDD
-func TestParseActionReturnsErr(t *testing.T) {
-	action, err := ParseAction("")
+func TestParseActionReturnsErrOnNonexistentFile(t *testing.T) {
+	action, err := ParseAction("testdata/non-existent-file.404")
 	require.Error(t, err)
 	require.Nil(t, action)
+}
+
+func TestParseActionParsesCompositeActionCorrectly(t *testing.T) {
+	action, err := ParseAction("testdata/composite-example.yaml")
+	require.NoError(t, err)
+	require.NotNil(t, action)
+
+	assert.True(t, action.IsComposite())
+}
+
+func TestParseActionParsesDockerActionCorrectly(t *testing.T) {
+	action, err := ParseAction("testdata/docker-example.yaml")
+	require.NoError(t, err)
+	require.NotNil(t, action)
+
+	assert.True(t, action.IsDocker())
+}
+
+func TestParseActionParsesJavascriptActionCorrectly(t *testing.T) {
+	action, err := ParseAction("testdata/js-example.yaml")
+	require.NoError(t, err)
+	require.NotNil(t, action)
+
+	assert.True(t, action.IsJavascript())
+}
+
+func TestAction_IsComposite(t *testing.T) {
+	type testData struct {
+		using    string
+		expected bool
+		message  string
+	}
+	tests := []testData{
+		{
+			using:    "composite",
+			expected: true,
+			message:  "expected composite to resolve as true",
+		},
+		{
+			using:    "node16",
+			expected: false,
+			message:  "expected node16 to resolve as false",
+		},
+		{
+			using:    "node20",
+			expected: false,
+			message:  "expected node20 to resolve as false",
+		},
+		{
+			using:    "docker",
+			expected: false,
+			message:  "expected docker to resolve as false",
+		},
+	}
+
+	for _, test := range tests {
+		action := Action{
+			Runs: &RunsBlock{
+				Using: test.using,
+			},
+		}
+		assert.Equal(t, test.expected, action.IsComposite(), test.message)
+	}
+}
+
+func TestAction_IsDocker(t *testing.T) {
+	type testData struct {
+		using    string
+		expected bool
+		message  string
+	}
+	tests := []testData{
+		{
+			using:    "composite",
+			expected: false,
+			message:  "expected composite to resolve as false",
+		},
+		{
+			using:    "node16",
+			expected: false,
+			message:  "expected node16 to resolve as false",
+		},
+		{
+			using:    "node20",
+			expected: false,
+			message:  "expected node20 to resolve as false",
+		},
+		{
+			using:    "docker",
+			expected: true,
+			message:  "expected docker to resolve as true",
+		},
+	}
+
+	for _, test := range tests {
+		action := Action{
+			Runs: &RunsBlock{
+				Using: test.using,
+			},
+		}
+		assert.Equal(t, test.expected, action.IsDocker(), test.message)
+	}
+}
+
+func TestAction_IsJavascript(t *testing.T) {
+	type testData struct {
+		using    string
+		expected bool
+		message  string
+	}
+	tests := []testData{
+		{
+			using:    "composite",
+			expected: false,
+			message:  "expected composite to resolve as false",
+		},
+		{
+			using:    "node16",
+			expected: true,
+			message:  "expected node16 to resolve as true",
+		},
+		{
+			using:    "node20",
+			expected: true,
+			message:  "expected node20 to resolve as true",
+		},
+		{
+			using:    "docker",
+			expected: false,
+			message:  "expected docker to resolve as false",
+		},
+	}
+
+	for _, test := range tests {
+		action := Action{
+			Runs: &RunsBlock{
+				Using: test.using,
+			},
+		}
+		assert.Equal(t, test.expected, action.IsJavascript(), test.message)
+	}
+}
+
+func TestAction_GetNodeVersion(t *testing.T) {
+	type testData struct {
+		using       string
+		expected    int
+		shouldError bool
+		message     string
+	}
+	tests := []testData{
+		{
+			using:       "composite",
+			expected:    0,
+			shouldError: true,
+			message:     "expected composite to resolve as 0, error",
+		},
+		{
+			using:       "node16",
+			expected:    16,
+			shouldError: false,
+			message:     "expected node16 to resolve as 16, nil",
+		},
+		{
+			using:       "node20",
+			expected:    20,
+			shouldError: false,
+			message:     "expected node20 to resolve as 20, nil",
+		},
+		{
+			using:       "nodefoo",
+			expected:    0,
+			shouldError: true,
+			message:     "expected node20 to resolve as 0, error",
+		},
+		{
+			using:       "docker",
+			expected:    0,
+			shouldError: true,
+			message:     "expected docker to resolve as 0, error",
+		},
+	}
+
+	for _, test := range tests {
+		action := Action{
+			Runs: &RunsBlock{
+				Using: test.using,
+			},
+		}
+		version, err := action.GetNodeVersion()
+		if test.shouldError {
+			assert.Error(t, err)
+		} else {
+			assert.NoError(t, err)
+		}
+		assert.Equal(t, test.expected, version)
+	}
 }

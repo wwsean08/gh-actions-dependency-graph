a85c563d42874a20be71ef12c3b0e89914ea9f4b
Sean Smith <sean@wwsean08.com>
2023-11-09 20:52:22 -0800

    remove unused error

--- pkg/scanner/errors.go
+++ pkg/scanner/errors.go
@@ -6,6 +6,5 @@ var (
 	NotApplicableError          = errors.New("scanner is not applicable to to this action")
 	UnexpectedResponseCodeError = errors.New("received an unexpected http response code")
 	LikelyShaError              = errors.New("reference is likely a sha-1 sum")
-	NotATagError                = errors.New("reference is not a tag")
 	LocalActionError            = errors.New("action is local, and so is not referred to by a git ref")
 )
